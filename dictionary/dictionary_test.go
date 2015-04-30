732196127be6dd8bf2926904ab3219996b577caf
Wang Bin <wangbin.zibo@gmail.com>
2015-04-30 11:21:16 +0800

    added more tests for dictionary.go

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -33,13 +33,33 @@ func TestLoadUserDictionary(t *testing.T) {
 	}
 }
 
+func TestFrequency(t *testing.T) {
+	d.LoadUserDictionary("../userdict.txt")
+	if f, _ := d.Frequency("八一双鹿"); f != 3.0 {
+		t.Fatalf("Wrong frequency for word  \"八一双鹿\", expect 3.0, got %f", f)
+	}
+
+}
+
 func TestTotal(t *testing.T) {
 	d.LoadDictionary("../userdict.txt")
-
 	if d.Total() != 319.0 {
 		t.Fatalf("Wrong total for userdict.txt, expect 319.0, got %f", d.Total())
 	}
+}
+
+func TestLogTotal(t *testing.T) {
+	d.LoadDictionary("../userdict.txt")
 	if d.LogTotal() != math.Log(319.0) {
 		t.Fatalf("Wrong total for userdict.txt, expect %f, got %f", math.Log(319.0), d.LogTotal())
+	}
+}
+
+func TestAddToken(t *testing.T) {
+	d.LoadDictionary("../userdict.txt")
+	token := Token{text: "超敏C反应蛋白", frequency: 100.0, pos: "nz"}
+	d.AddToken(token)
+	if f, _ := d.Frequency("超敏C反应蛋白"); f != 100.0 {
+		t.Fatalf("Failed to add Token \"超敏C反应蛋白\", except frequency 100.0, got %f", f)
 	}
 }
