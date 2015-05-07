7440fa00df439477662c210f7538522e34d9c700
Wang Bin <wangbin.zibo@gmail.com>
2015-05-07 17:43:35 +0800

    tweak examples for nalyse

--- analyse/example_test.go
+++ analyse/example_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/wangbin/jiebago/analyse"
 )
 
-func ExampleExtractTags() {
+func Example_extractTags() {
 	var t analyse.TagExtracter
 	t.LoadDictionary("../dict.txt")
 	t.LoadIdf("idf.txt")
@@ -21,7 +21,7 @@ func ExampleExtractTags() {
 	// Top 5 tags: Python / C++ / 伸手不见五指 / 孙悟空 / 黑夜 /
 }
 
-func ExampleTextRank() {
+func Example_textRank() {
 	var t analyse.TextRanker
 	t.LoadDictionary("../dict.txt")
 	sentence := "此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。"
